df183377dc00d8d8b9f79130e1ec4f4b3d8989b7
ayushpathak477 <ayushpathak887@gmail.com>
2025-02-02 18:07:53 +0530

    added css properties for product display page

--- frontend/src/Components/ProductDisplay/ProductDisplay.jsx
+++ frontend/src/Components/ProductDisplay/ProductDisplay.jsx
@@ -9,10 +9,10 @@ const ProductDisplay = (props) => {
     <div className='productdisplay'>
         <div className="productdisplay-left">
             <div className="productdisplay-img-list">
-                <img src={product.img} alt="" />
-                <img src={product.img} alt="" />
-                <img src={product.img} alt="" />
-                <img src={product.img} alt="" />
+                <img src={product.image} alt="" />
+                <img src={product.image} alt="" />
+                <img src={product.image} alt="" />
+                <img src={product.image} alt="" />
             </div>
             <div className="productdisplay-img">
                 <img className='productdisplay-main-img' src={product.image} alt="" />
@@ -20,7 +20,7 @@ const ProductDisplay = (props) => {
         </div>
         <div className="productdisplay-right">
             <h1>{product.name}</h1>
-            <div className="productdisplay-right-star">
+            <div className="productdisplay-right-stars">
                 <img src={star_icon} alt="" />
                 <img src={star_icon} alt="" />
                 <img src={star_icon} alt="" />
@@ -37,7 +37,7 @@ const ProductDisplay = (props) => {
             </div>
             <div className="productdisplay-right-size">
                 <h1>Select Size</h1>
-                <div className="productdisplay-right-size"> 
+                <div className="productdisplay-right-sizes"> 
                     <div>S</div>
                     <div>M</div>
                     <div>L</div>
