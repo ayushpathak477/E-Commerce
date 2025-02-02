2966208ca1e8918bf2e959cf8634b5ea2c997ddd
ayushpathak477 <ayushpathak887@gmail.com>
2025-02-02 08:56:26 +0530

    added product page functionality

--- frontend/src/Pages/Product.jsx
+++ frontend/src/Pages/Product.jsx
@@ -1,7 +1,18 @@
-import React from "react";
+import React, { useContext } from "react";
+import Breadcrum from "../Components/Breadcrums/Breadcrum";
+import { ShopContext } from '../Context/ShopContext'
+import { useParams } from "react-router-dom";
+
 
 const Product = () => {
-  return <div></div>;
+  const {all_product} = useContext(ShopContext);
+  const {productId} = useParams();
+  const product = all_product.find((e)=> e.id === Number(productId))
+  return (
+  <div>
+    <Breadcrum product = {product}/>
+  </div>
+  )
 };
 
 export default Product;
